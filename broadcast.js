d97d4b150ba32400500c9f220d65b6d77c7efc90
marius321967 <marius.dras@gmail.com>
2018-05-24 21:50:51 +0300

    Event broadcast server now uses created services

--- broadcast.js
+++ broadcast.js
@@ -1,5 +1,10 @@
 const WebSocket = require('ws');
 const http = require('http');
+const state = require('./state');
+const parseWsMessage = require('./services/broadcast/parseWsMessage');
+const registerListener = require('./services/broadcast/registerListener');
+const sendWsMessage = require('./services/broadcast/sendWsMessage');
+
 /**
  * Setup WebSocket boardcast server to distribute events to listeners.
  */
@@ -7,11 +12,30 @@ module.exports = () => {
     const server = http.createServer();
     const wss = new WebSocket.Server({ server });
     
-    wss.on('connection', function connection(ws) {
-        ws.on('message', function incoming(message) {
-            console.log('received: %s', message);
-            ws.send('Xx'+message+'xX');
+    wss.on('connection', (connection) => {
+        // Handle initialization message.
+        connection.on('message', (serializedMessage) => {
+            
+            parseWsMessage(serializedMessage)
+                .then(message => {
+                    return registerListener(connection, message.events, state);
+                })
+                // Send OK or error.
+                .then(() => {
+                    return sendWsMessage({
+                        type: 'init',
+                        code: 'OK'
+                    }, connection);
+                })
+                .catch(err => {
+                    return sendWsMessage({
+                        type: 'init',
+                        code: err.message
+                    }, connection);
+                });
+
         });
     });
+
     return {httpSrv: server, wsSrv: wss};
 }
