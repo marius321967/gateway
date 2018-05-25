91467d4918b616fea86933c2ba94d99cd0d4dab9
marius321967 <marius.dras@gmail.com>
2018-05-25 10:31:50 +0300

    listen server now authenticates

--- listen.js
+++ listen.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const state = require('./state');
 const notifyListeners = require('./services/listen/notifyListeners');
+const authenticate = require('./services/auth/authenticate');
 
 /**
  * Setup listen server with Express.
@@ -11,11 +12,19 @@ module.exports = () => {
     app.use(express.json());
     // Setup routes.
     app.post('/event', (req, res) => {
-        notifyListeners(req.body, state)
+        authenticate(req.headers.authorization)
+            .then(() => {
+                return notifyListeners(req.body, state);
+            })
             .then(() => {
                 res.send({ code: 'OK' });
             })
-            .catch(() => {
+            .catch(err => {
+                const msg = err.message;
+                if (msg === 'UNAUTHENTICATED' || msg === 'AUTH_UNRECOGNIZED_SCHEME') {
+                    res.statusCode = 403;
+                    return res.send({code: msg})
+                }
                 res.statusCode = 400;
                 res.send({ code: "INVALID_BODY" })
             })
