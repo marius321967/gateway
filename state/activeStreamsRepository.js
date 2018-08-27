589c9b4b26fdc62b1de6a2c0d6b2d8afb58bde80
Marius Drąsutis <marius.dras@gmail.com>
2018-08-27 08:50:57 +0300

    Added repository for active streams

---
+++ state/activeStreamsRepository.js
@@ -0,0 +1,46 @@
+const Stream = require('../models/Stream');
+const uuid = require('uuid');
+
+const streams = {};
+const streamsByType = {};
+
+const ensureListsCreated = (type) => {
+    if (streamsByType[type] == null)
+        streamsByType[type] = [];
+}
+
+/**
+ * Add new stream to the active streams repository.
+ * @param {Stream} stream
+ * @returns {String} ID assigned to the stream.
+ */
+exports.addStream = (stream) => {
+    const id = uuid();
+    stream.id = id;
+    streams[id] = stream;
+    ensureListsCreated(stream.type);
+    streamsByType[stream.type].push(stream);
+    return id;
+}
+
+exports.getStreamById = (id) => {
+    return streams[id];
+}
+
+/**
+ * Get all streams of the given data type.
+ * @param {String} type Data type.
+ */
+exports.getStreamsByType = (type) => {
+    ensureListsCreated(type);
+    return streamsByType[type];
+}
+
+/**
+ * Remove stream from the active streams repository.
+ * @param {Stream} stream 
+ */
+exports.removeStream = (stream) => {
+    delete streams[stream.id];
+    streamsByType[stream.type].splice(streamsByType[stream.type].indexOf(stream), 1);
+}
