80ee61630db611385772067b0394222ef5da1a24
Marius Drąsutis <marius.dras@gmail.com>
2018-08-30 20:18:17 +0300

    Removed redundant state.js in favour of separate repository objects

--- state.js
+++
@@ -1,9 +0,0 @@
-/**
- * Gobal app state.
- * listeners: each key in the object will be name of event. 
- *  Content will be an array of WebSocket connections which are allowed to listen
- *  to event.
- */
-module.exports = {
-    listeners: {}
-};
