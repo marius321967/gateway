6072544ffbe02bdc94a15193861c78c8714d874f
Marius Drąsutis <marius.dras@gmail.com>
2018-09-01 14:55:43 +0300

    Comments

--- routes/pushEvent.js
+++ routes/pushEvent.js
@@ -2,13 +2,20 @@ const notifyListeners = require('../services/broadcast/notifyListeners');
 const authenticate = require('../services/auth/authenticate');
 const verifyAuthorization = require('../services/auth/verifyAuthorization');
 
+/**
+ * Route for pushing an event to Gateway.
+ * Authenticates, authorizes, then retrieves all active listeners
+ * and sends the event to them.
+ * @param {*} req 
+ * @param {*} res 
+ */
 module.exports = (req, res) => {
     authenticate(req.headers.authorization)
         .then(token => {
             return verifyAuthorization(
-                    token, 
-                    { events: [req.body.type] }, 
-                    'write');
+                token, 
+                { events: [req.body.type] }, 
+                'write');
         })
         .then(() => {
             return notifyListeners('events', req.body.type, 
@@ -24,9 +31,8 @@ module.exports = (req, res) => {
             const msg = err.message;
             if (msg === 'UNAUTHENTICATED' || msg === 'AUTH_UNRECOGNIZED_SCHEME' ||
                 msg.startsWith('DATA_TYPE_UNAUTHORIZED')) {
-                // console.warn('Warning: unauthenticated request from IP '+req.connection.remoteAddress);
                 res.statusCode = 403;
-                return res.send({code: msg})
+                return res.send({ code: msg })
             }
             res.statusCode = 400;
             res.send({ code: "INVALID_BODY" })
