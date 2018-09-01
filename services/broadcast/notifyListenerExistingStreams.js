97c07e85319412e3888afb280794d1387d9c32f8
Marius Drąsutis <marius.dras@gmail.com>
2018-09-01 14:56:34 +0300

    Commenting

--- services/broadcast/notifyListenerExistingStreams.js
+++ services/broadcast/notifyListenerExistingStreams.js
@@ -2,6 +2,11 @@ const activeStreamsRepository = require('../../state/activeStreamsRepository');
 const notifyListener = require('./notifyListener');
 const constructStreamNotification = require('./constructStreamNotification');
 
+/**
+ * Send notification to the listener about each existing stream.
+ * This service is used to notify a listener who has just connected.
+ * @param {*} listener 
+ */
 module.exports = (listener) => {
     // For each stream the listener is subscribed to...
     for (const typeIndex in listener.dataTypes.streams) {
