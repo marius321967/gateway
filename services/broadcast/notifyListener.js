5cf4f99af8b8fc643daf7b013cb335d580eb1aba
Marius Drąsutis <marius.dras@gmail.com>
2018-09-03 20:29:50 +0300

    Updated notifyListener services with new message structure

--- services/broadcast/notifyListener.js
+++ services/broadcast/notifyListener.js
@@ -7,21 +7,19 @@ const sendWsMessage = require('../listen/sendWsMessage');
  * category and data_type fields.
  * Resolves with null. Not expected to reject.
  * @param {Listener} listener Whom to inform.
- * @param {String} category Data category (event, stream).
- * @param {String} type Data type.
+ * @param {String} messageType 'type' property in the root object of the message
+ * that will be sent over Websocket. This is usually the singular version of the
+ * data category.
  * @param {*} payload This payload will be sent, in addition to data category and type.
  * @return {Promise}
  */
-module.exports = (listener, category, type, payload) => {
+module.exports = (listener, messageType, payload) => {
     return new Promise((resolve, reject) => {
-        // Merge payload and message parameters.
-        const message = Object.assign(
-            payload, 
-            {
-                type: 'data',
-                category: category,
-                data_type: type,
-            })
+        // Root message object.
+        const message = {
+            type: messageType,
+            payload: payload
+        };
         sendWsMessage(message, listener.connection);
         return resolve();
     });
--- services/broadcast/notifyListeners.js
+++ services/broadcast/notifyListeners.js
@@ -8,16 +8,19 @@ const notifyListener = require('./notifyListener');
  * Resolves with null. Not expected to reject.
  * @param {String} category Data category (event, stream).
  * @param {String} type Data type.
+ * @param {String} messageType 'type' property in the root object of the message
+ * that will be sent over Websocket. This is usually the singular version of the
+ * data category.
  * @param {*} payload This payload will be sent, in addition to data category and type.
  * @return {Promise}
  */
-module.exports = (category, type, payload) => {
+module.exports = (category, type, messageType, payload) => {
     return new Promise((resolve, reject) => {
         // Retrieve listeners for this data type.
         const listeners = listenerRepository.getListeners(category, type);
         // Notify all listeners.
         for (let i in listeners)
-            notifyListener(listeners[i], category, type, payload);
+            notifyListener(listeners[i], messageType, payload);
         return resolve();
     });
 };
