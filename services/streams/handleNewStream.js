0e5da2dc94d110bd11dc8142cce0e97c0f3e9bf7
Marius Drąsutis <marius.dras@gmail.com>
2018-09-03 20:31:31 +0300

    Fixed stream handler duplicate notification, updated for new data struct

--- services/streams/handleNewStream.js
+++ services/streams/handleNewStream.js
@@ -2,7 +2,7 @@ const Stream = require('../../models/Stream');
 const activeStreamsRepository = require('../../state/activeStreamsRepository');
 const listenerRepository = require('../../state/listenerRepository');
 const notifyListeners = require('../../services/broadcast/notifyListeners');
-const constructStreamNotification = require('./constructStreamNotification')
+const constructStreamNotificationPayload = require('./constructStreamNotificationPayload')
 const devnull = require('dev-null');
 
 /**
@@ -13,18 +13,19 @@ const devnull = require('dev-null');
  */
 module.exports = (request, type, metadata) => {
     const stream = new Stream(request, type, metadata);
+    // Add stream to the actives streams repository.
+    // Streams must be kept in a repository so that new listeners are
+    // notified about already existing streams.
     activeStreamsRepository.addStream(stream);
-    // Notify listeners.
-    const listeners = listenerRepository.getListeners('streams', type);
-    for (let i in listeners) {
-        notifyListeners('streams', type, constructStreamNotification(stream));
-    }
-    // Keep stream alive, otherwise piping to listeners will not work.
+    // Notify listeners about the new stream - do it asychronousy.
+    notifyListeners('streams', type, 'stream', constructStreamNotificationPayload(stream));
+    // Keep stream alive by piping to some empty reader, 
+    // otherwise piping to listeners will not work.
     request.pipe(devnull());
-    // request.pipe(process.stdout);
-    // Handle stream end.
+    // Set handler for when the stream ends.
+    // This handler is called both when the stream ends gracefully
+    // and when it crashes.
     request.socket.on('close', () => {
-        // Stream closed gracefully or crashed.
         activeStreamsRepository.removeStream(stream);
-    })
+    });
 }
