edcd6d1ce8a5badb6a265a9e0702860613cd0fa4
Marius Drąsutis <marius.dras@gmail.com>
2018-09-13 19:13:09 +0300

    Comments

--- baseServer.js
+++ baseServer.js
@@ -3,9 +3,11 @@ const http = require('http');
 const https = require('https');
 
 /**
- * Checks whether HTTPS_ENABLE is 'true', then returns a createServer() instance
- * of either an HTTP or HTTPS server. Provides optional app to createServer(). 
- * If HTTPS, loads /certs/cert.key and /certs/cert.crt files.
+ * Returns nodejs http.createServer() instance of either an HTTP or 
+ * HTTPS server. Depending on the HTTPS_ENABLE environment variable
+ * Provides optional app to createServer().
+ * 
+ * If HTTPS is enabled, loads /certs/cert.key and /certs/cert.crt files.
  * @param {*} app Optional app argument to provide to createServer().
  */
 module.exports = (app) => {
